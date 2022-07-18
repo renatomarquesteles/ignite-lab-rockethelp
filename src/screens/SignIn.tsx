c5334fc8f471ec3062218949f24df20619f8a3e2
Renato <renatomarquesteles@gmail.com>
2022-07-17 21:27:50 -0300

    configure use of SVGs

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,8 +1,11 @@
 import { Heading, VStack } from 'native-base';
 
+import Logo from '../assets/logo_primary.svg';
+
 export const SignIn = () => {
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
       <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
         Access your account
       </Heading>
