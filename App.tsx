1952dde477d97e5d9aeb5d9b7b512f4bcaacc909
Renato <renatomarquesteles@gmail.com>
2022-07-17 21:21:44 -0300

    configure custom font Roboto

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
+import { Loading } from './src/components/Loading';
 
 import { SignIn } from './src/screens/SignIn';
 import { THEME } from './src/styles/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <SignIn />
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,9 +1,11 @@
-import { View, Text } from 'react-native';
+import { Heading, VStack } from 'native-base';
 
 export const SignIn = () => {
   return (
-    <View>
-      <Text>Hello world</Text>
-    </View>
-  )
-}
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Access your account
+      </Heading>
+    </VStack>
+  );
+};
